392fea2edb6b6975db1554cbec4705652078b7c3
Dinh truong Lam <mra2322001@gmail.com>
2024-12-20 11:25:51 +0300

    refactor: clean up project structure and remove unused files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,18 @@
+import { useState } from "react";
 
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      className="text-red-900"
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      lam dep trai
+      {count}
+    </div>
+  );
+}
+
+export default App;
